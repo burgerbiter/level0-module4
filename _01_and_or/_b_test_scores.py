7df398cedfad26b7a68702f93385728ae9a9db3b
league <burgerbiter>
2022-10-21 18:24:28 -0700

    Worked on

--- _01_and_or/_b_test_scores.py
+++ _01_and_or/_b_test_scores.py
@@ -13,14 +13,29 @@ from tkinter import messagebox, simpledialog, Tk
 #   D = 59.5 to less than 69.5
 #   F = less than 59.5
 if __name__ == '__main__':
+    window = Tk()
+    window.withdraw()
     # TODO) Ask the user for their score on the FIRST test and store their
     #  score in a variable
-
+    T1 = simpledialog.askinteger(None, prompt="1st Test Score")
     # TODO) Ask the user for their score on the SECOND test and store their
     #  score in a variable
-
+    T2 = simpledialog.askinteger(None, prompt="2nd Test Score")
     # TODO) Take the average score of both tests (total score / 2)
-
+    sum = T1 + T2
+    ave = sum/2
+    if ave > 100:
+        print("Don't lie.")
+    elif ave >= 89.5:
+        print("Good job!")
+    elif ave >= 79.5:
+        print("Average.")
+    elif ave >= 69.5:
+        print("You passed, but try harder next time.")
+    elif ave >= 59.5:
+        print("You barely passed.")
+    elif ave <59.5:
+        print("You failed.")
     # TODO) Use if statements to check the average score and print the
     #  corresponding letter grade back to the user. Also, give a different
     #  message according to their grade. Example, for an 'A' grade:
--- _02_boolean/_b_sleepy_head.py
+++ _02_boolean/_b_sleepy_head.py
@@ -2,7 +2,7 @@
 Use boolean variables to control program logic between two different code
 paths.
 """
-
+import turtle
 
 if __name__ == '__main__':
     # TODO)
@@ -17,4 +17,28 @@ if __name__ == '__main__':
     #  4. Use two boolean variables, one to indicate if a shape should be red,
     #     the other to indicate if the shape is to be square. When both
     #     variables are true, use a turtle to draw a red square.
+    weekend = True
+    passed = False
+    game = True
+    color = True
+    shape = True
+
+    if weekend:
+        print("It's the weekend!")
+    else:
+        print("Time for school.")
+    if passed:
+        print("Good job!")
+    else:
+        print("nice try.")
+    if game:
+        print("Game Over")
+    if color and shape:
+        tat = turtle.Turtle()
+        tat.pencolor('red')
+        for i in range(4):
+            tat.forward(100)
+            tat.left(90)
+
+
     pass
